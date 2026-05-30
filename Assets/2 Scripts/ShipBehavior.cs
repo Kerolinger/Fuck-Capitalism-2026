039e0ea5e44fe93e347d389a1dc5bbcd1fea9548
polinazvezdohliad <104728375+polinazvezdohliad@users.noreply.github.com>
2026-05-30 20:09:55 +0200

    mian scene and ship script - added sounds

--- Assets/2 Scripts/ShipBehavior.cs
+++ Assets/2 Scripts/ShipBehavior.cs
@@ -8,22 +8,30 @@ public class ShipBehavior : MonoBehaviour
 
     [SerializeField] private GameObject[] containers;
 
+    [Header("Audio")]
+    [SerializeField] private AudioSource successSound;
+    [SerializeField] private AudioSource failureSound;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Trash"))
         {
+            failureSound.Play();
             gameManager.LooseContainer();
             trashGrid.SetShipSpeed(0);
         }
         else if (other.CompareTag("Haven"))
         {
+            successSound.Play();
             StartCoroutine(HavenArrival());
+           
         }
     }
 
     IEnumerator HavenArrival()
     {
         trashGrid.SetShipSpeed(99);
+        successSound.Play();
         yield return new WaitForSeconds(0.75f);
         gameManager.StartLevelTransition();
 
@@ -38,12 +46,11 @@ public class ShipBehavior : MonoBehaviour
             else
                 containers[i].SetActive(false);
 
-            if(lostFreshContainer && i ==  remainingContainers)
+            if (lostFreshContainer && i == remainingContainers)
             {
                 containers[i].SetActive(true);
                 containers[i].transform.parent = trashGrid.Haven.transform;
-                containers[i].transform.position = new Vector3(-215f,-51.5f,130f);
-                containers[i].transform.eulerAngles = new Vector3(-0, -20, 5f);
+                containers[i].transform.position = new Vector3(-215f, -61.5f, 130f);
             }
         }
     }
